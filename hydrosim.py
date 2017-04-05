f52eb3baa784df08b3a3669dd4aa60eb219f7bf5
scjethro <alastairmarshall@Alastairs-MacBook-Pro.local>
2017-04-05 15:55:40 +0100

    comments and few changes

--- hydrosim.py
+++ hydrosim.py
@@ -33,6 +33,13 @@ timestep = 0.001
 cellArray = []
 n = 1000
 
+# @brief calculate the flux through all the surfaces
+#
+# made into a function taking everything needed
+# use the variable test to define 1 or 0 and thus whether you should use + or - on the flux
+# after updating variables it calculates the primitive variables again, a step I forgot previously
+#
+
 def fluxCalc(cell, test, massFlux, momFlux, energyFlux):
     area = cell._surfaceArea
 
@@ -41,13 +48,14 @@ def fluxCalc(cell, test, massFlux, momFlux, energyFlux):
         cell._mom -= momFlux * area * timestep
         cell._energy -= energyFlux * area * timestep
 
-    else:
+    elif(test == 0):
         cell._mass += massFlux * area * timestep
         cell._mom += momFlux * area * timestep
         cell._energy += energyFlux * area * timestep
 
     cell.calcPrims()
 
+# look over the number of cells and populate the list
 
 for i in range(0,n):
     cell = Cell()
@@ -61,32 +69,45 @@ for i in range(0,n):
     else:
         cell._mass = 0.00125
         cell._energy = 0.001/(gamma - 1)
+
+    # using the defined method to calculate the initial primitive variables
     cell.calcPrims()
     cellArray.append(cell)
 
+# loop over all the cells again and set all the left neighbours to the cells on the right
+# I tried to do this within the loop above but couldn't get it to work
 for i in range(0, len(cellArray)):
     cellArray[i-1]._rightCell = cellArray[i]
 
+# create the Riemann solver from bwvdnbro/python_finite_volume_solver
 solver = riemannsolver.RiemannSolver(gamma)
 
-for step in range(0, 200):
+# define a maximum number of timesteps
+maxT = 200;
+
+# loop over the timesteps to repeatedly calculate the Riemann problem and work out the results
+for step in range(0, maxT):
     for cell in cellArray:
+        # per cell we find the right hand cell
         rightCell = cell._rightCell
 
+        # using the given Riemann solver we solve the Riemann equation
         densSol, velSol, pressSol, _ = solver.solve(cell._density,cell._velocity,cell._pressure,rightCell._density, rightCell._velocity, rightCell._pressure)
 
+        # calculate the fluxes as given in the equations we derived in class, again thanks to bwvdnbro
         massFlux = densSol*velSol
         momFlux = densSol*velSol**2+pressSol
         energyFlux = (pressSol*gamma/(gamma-1)+0.5*densSol*velSol**2)*velSol
 
+        # use the defined function above to calculate the fluxes
+        # everything is taken care of in the function
         fluxCalc(cell, 1,massFlux,momFlux,energyFlux)
         fluxCalc(rightCell, 0,massFlux,momFlux,energyFlux)
 
+# loops to just extract the density and position in an easy manner for plotting
 x = [i for i in range(0,n)]
 density = [cell._density for cell in cellArray]
 
-# print("x len", len(x))
-# print("density len", len(density))
-
+# plot the results as a scatter plot to see the output
 # plt.scatter(density,x)
 # plt.show()
